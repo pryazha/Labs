925c9e4b9cf1a6da96c4c3119d8fba592fdcb266
Ivan Pryadein <pryadeiniv@mail.ru>
2023-04-01 11:10:57 +0500

    Some changes

--- test.cpp
+++ test.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+struct MyClass
+{
+private:
+	string str;
+public:
+	void Set_Str(string str) {
+		this->str = str;
+	}
+
+	void Show_Str() {
+		cout << this->str << endl;
+	}
+
+	MyClass(string str) {
+		this->str = str;
+	}
+};
 
 int main()
 {
-	cout << "Test" << endl;
+	MyClass item("This is first item");	
+
+	item.Show_Str();
+
+	item.Set_Str("No, no, no, THIS is first item");
+
+	item.Show_Str();
+
 	return 0;
 }
