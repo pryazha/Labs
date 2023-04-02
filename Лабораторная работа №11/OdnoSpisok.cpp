f4a1c0b7145380a044922d0784bbe72118cd4393
Ivan Pryadein <pryadeiniv@mail.ru>
2023-04-02 11:14:00 +0500

    del some files

---
+++ Лабораторная работа №11/OdnoSpisok.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Тест\n";
+
+	system("pause");
+	return 0;
+}
---
+++ Лабораторная работа №11/Односвязный список.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	std::cout << "Тест\n";
+
+	system("pause");
+	return 0;
+}
