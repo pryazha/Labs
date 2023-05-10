32b88a8f392cde1274a6234253c2e4058b61957d
Ivan Pryadein <pryadeiniv@mail.ru>
2023-05-10 23:17:49 +0500

    lab 11

---
+++ Курс 1/Семестр 2/ООП/Лабораторная работа №11/Lab_11.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <list>
+#include <cstdlib>
+
+using namespace std;
+
+typedef list<double> List;
+
+List make_list(int n) {
+	List tempList;
+	for (int i = 0; i < n; i++) {
+		double tempElement = rand() % 11;
+		tempList.push_back(tempElement);
+	}
+	return tempList;
+}
+
+void print_list(List lis) {
+	list<double>::iterator it = lis.begin();
+	cout << "Data: ";
+	for (it; it != lis.end(); it++)
+		cout << *it << " ";
+	cout << "\n";
+}
+
+void insert_element(List& lis, double key) {
+	list<double>::iterator it = lis.begin();
+	for (it; it != lis.end(); it++)
+		if (*it == key)
+			break;
+	if (it == lis.end()) {
+		cout << "Element doesn't exist\n";
+		return;
+	}
+	int num, i = 0;
+	cout << "Number: ";
+	cin >> num;
+	for (it = lis.begin(); it != lis.end(); it++) {
+		if (num == (i + 1))
+			break;
+		i++;
+	}
+	lis.insert(++it, key);
+}
+void delete_element(List& lis, double key) {
+	list<double>::iterator it = lis.begin();
+	for (it; it != lis.end(); it++)
+		if (*it == key)
+			break;
+	if (it == lis.end()) {
+		cout << "Element doesn't exist\n";
+		return;
+	}
+	lis.erase(it);
+}
+
+void element_reduction(List& lis) {
+	double max, min;
+	list<double>::iterator it;
+	for (it)
+}
+
+
+int main() {
+	try {
+		list<double> li;
+		list<double>::iterator it = li.begin();
+		int numberOfElements;
+		cout << "Number of elements: ";
+		cin >> numberOfElements;
+		li = make_list(numberOfElements);
+		print_list(li);
+
+		double key;
+		cout << "Data of element: ";
+		cin >> key;
+		insert_element(li, key);
+		print_list(li);
+
+		cout << "Data of del element: ";
+		cin >> key;
+		delete_element(li, key);
+		print_list(li);
+	}
+	catch (int) {
+		cout << "Error!";
+	}
+	return 0;
+}
