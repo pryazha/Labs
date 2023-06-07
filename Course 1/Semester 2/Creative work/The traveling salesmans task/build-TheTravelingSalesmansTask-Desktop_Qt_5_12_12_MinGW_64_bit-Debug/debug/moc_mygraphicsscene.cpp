f1077116b40fb1bc2c5332326e140ffbb62c6a95
Ivan Pryadein <pryadeiniv@mail.ru>
2023-06-07 15:19:14 +0500

    changed TSP

---
+++ Course 1/Semester 2/Creative work/The traveling salesmans task/build-TheTravelingSalesmansTask-Desktop_Qt_5_12_12_MinGW_64_bit-Debug/debug/moc_mygraphicsscene.cpp
@@ -0,0 +1,165 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mygraphicsscene.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.12.12)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "C:/Pryazha/Programs/C++ programs/Creative-Work/Creative work/The traveling salesmans task/TheTravelingSalesmansTask/mygraphicsscene.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mygraphicsscene.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.12.12. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_myGraphicsScene_t {
+    QByteArrayData data[6];
+    char stringdata0[64];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_myGraphicsScene_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_myGraphicsScene_t qt_meta_stringdata_myGraphicsScene = {
+    {
+QT_MOC_LITERAL(0, 0, 15), // "myGraphicsScene"
+QT_MOC_LITERAL(1, 16, 11), // "city_chosen"
+QT_MOC_LITERAL(2, 28, 0), // ""
+QT_MOC_LITERAL(3, 29, 5), // "City*"
+QT_MOC_LITERAL(4, 35, 13), // "item_selected"
+QT_MOC_LITERAL(5, 49, 14) // "QGraphicsItem*"
+
+    },
+    "myGraphicsScene\0city_chosen\0\0City*\0"
+    "item_selected\0QGraphicsItem*"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_myGraphicsScene[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       2,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       2,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    1,   24,    2, 0x06 /* Public */,
+       4,    1,   27,    2, 0x06 /* Public */,
+
+ // signals: parameters
+    QMetaType::Void, 0x80000000 | 3,    2,
+    QMetaType::Void, 0x80000000 | 5,    2,
+
+       0        // eod
+};
+
+void myGraphicsScene::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<myGraphicsScene *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->city_chosen((*reinterpret_cast< City*(*)>(_a[1]))); break;
+        case 1: _t->item_selected((*reinterpret_cast< QGraphicsItem*(*)>(_a[1]))); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+        case 1:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QGraphicsItem* >(); break;
+            }
+            break;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (myGraphicsScene::*)(City * );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&myGraphicsScene::city_chosen)) {
+                *result = 0;
+                return;
+            }
+        }
+        {
+            using _t = void (myGraphicsScene::*)(QGraphicsItem * );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&myGraphicsScene::item_selected)) {
+                *result = 1;
+                return;
+            }
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject myGraphicsScene::staticMetaObject = { {
+    &QGraphicsScene::staticMetaObject,
+    qt_meta_stringdata_myGraphicsScene.data,
+    qt_meta_data_myGraphicsScene,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *myGraphicsScene::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *myGraphicsScene::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_myGraphicsScene.stringdata0))
+        return static_cast<void*>(this);
+    return QGraphicsScene::qt_metacast(_clname);
+}
+
+int myGraphicsScene::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QGraphicsScene::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 2)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 2)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void myGraphicsScene::city_chosen(City * _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
+}
+
+// SIGNAL 1
+void myGraphicsScene::item_selected(QGraphicsItem * _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 1, _a);
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
