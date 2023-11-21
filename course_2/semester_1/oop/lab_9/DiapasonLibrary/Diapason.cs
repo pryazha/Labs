dab63b9bf08441f56f61449034ca2be42211f8db
Ivan Pryadein <pryadeiniv@mail.ru>
2023-11-21 16:04:06 +0500

    add lab_7 databases

---
+++ course_2/semester_1/oop/lab_9/DiapasonLibrary/Diapason.cs
@@ -0,0 +1,19 @@
+﻿namespace UtilityLibraries;
+
+public class DiapasonLibrary
+{
+    public DiapasonLibrary() {
+    }
+
+    public DiapasonLibrary(int minRange, int maxRange) {
+    }
+
+    ~DiapasonLibrary() {
+    }
+
+    public static bool InRange(int number, int minRange, int maxRange) {
+        return number > minRange && number < maxRange;
+    }
+
+    private int minRange, maxRange;
+}
